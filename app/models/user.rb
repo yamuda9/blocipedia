6403da5d4a48cb0526377c01012ee616df2992c6
Alvin Andal <alvin.009.andal@gmail.com>
2016-02-25 21:43:04 +0000

    Implemented User Roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_many :wikis
+  
+  before_save { self.role ||= :standard }
+  
+  enum role: [:standard, :premium, :admin]
 end
